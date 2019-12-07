7110d304ff1e6bed10edae6dbe0027d78318dff9
Nikeweke <eamil>
2019-12-07 18:36:23 +0200

    new upd

---
+++ examples/workdir_not_changing_host_works/examples/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('How are you doing');
+});
+
+const PORT = 8080
+app.listen(PORT, () => {
+  console.log('Listening on port 8080');
+});
+
+
+dsadsda
